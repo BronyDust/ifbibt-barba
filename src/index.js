0cece8384dc9173b2dc6ffc426c2ec8b9a73bd70
Ryan Wright <SivanoFFDS@gmail.com>
2020-08-19 02:53:59 +0700

    Placeholder content to check simpliest barba transition

--- src/index.js
+++ src/index.js
@@ -1,13 +1,36 @@
 import barba from '@barba/core';
+import gsap, {Power2} from 'gsap';
 import './content.css';
 import './nav.css';
 import './styles.css';
 
 document.addEventListener('DOMContentLoaded', () => {
+
+
     barba.init({
         sync: true,
         transitions: [{
-    
+            name: 'default-transition',
+            leave(data) {
+                return gsap.fromTo(data.current.container, .2, {
+                    x: 0,
+                    opacity: 1
+                }, {
+                    x: -100,
+                    opacity: 0,
+                    ease: Power2.easeOut
+                });
+            },
+            enter(data) {
+                return gsap.fromTo(data.next.container, .2, {
+                    x: 100,
+                    opacity: 0
+                }, {
+                    x: 0,
+                    opacity: 1,
+                    ease: Power2.easeOut
+                });
+            }
         }]
     });
 });
