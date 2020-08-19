3424f17a73405495cea4760c40f8362d6529a0c6
Ryan Wright <SivanoFFDS@gmail.com>
2020-08-19 14:58:36 +0700

    Complate barba transitions

--- src/index.js
+++ src/index.js
@@ -4,33 +4,97 @@ import './content.css';
 import './nav.css';
 import './styles.css';
 
-document.addEventListener('DOMContentLoaded', () => {
+function closeMenuOnAnyClick() {
+    const nav = document.querySelector('.global-nav-items');
+    const openTrigger = document.getElementById('openmobile');
+    nav.addEventListener('click', () => {
+        openTrigger.checked = false;
+    });
+}
 
+document.addEventListener('DOMContentLoaded', () => {
+    closeMenuOnAnyClick();
 
     barba.init({
-        sync: true,
+        debug: true,
         transitions: [{
-            name: 'default-transition',
-            leave(data) {
-                return gsap.fromTo(data.current.container, .2, {
-                    x: 0,
-                    opacity: 1
+            name: 'enter-animation',
+            once: ({next}) => new Promise((res, rej) => {
+                gsap.fromTo(next.container.querySelector('.global-nav'), .2, {
+                    y: -50,
+                    opacity: 0
                 }, {
-                    x: -100,
-                    opacity: 0,
+                    y: 0,
+                    opacity: 1
+                });
+                gsap.fromTo(next.container.querySelectorAll('.content-block-header > *'), .5, {
+                    y: 100,
+                    opacity: 0
+                }, {
+                    y: 0,
+                    opacity: 1,
+                    stagger: .1,
                     ease: Power2.easeOut
                 });
-            },
-            enter(data) {
-                return gsap.fromTo(data.next.container, .2, {
-                    x: 100,
+                gsap.fromTo(next.container.querySelector('.content-bg'), .5, {
+                    opacity: 0
+                }, {
+                    opacity: 1
+                });
+                setTimeout(() => res(), 500);
+            }),
+            leave: ({current}) => new Promise((res, rej) => {
+                const mult = current.namespace === 'home' ? -1 : 1;
+
+                gsap.fromTo(current.container.querySelector('.global-nav'), .2, {
+                    y: 0,
+                    opacity: 1
+                }, {
+                    y: -50,
+                    opacity: 0
+                });
+                gsap.fromTo(current.container.querySelectorAll('.content-block-header > *'), .3, {
+                    x: 0,
+                    opacity: 1,
+                }, {
+                    x: 100 * mult,
+                    opacity: 0,
+                    stagger: .05,
+                    ease: Power2.easeOut
+                });
+                gsap.fromTo(current.container.querySelector('.content-bg'), .3, {
+                    opacity: 1
+                }, {
+                    opacity: 0
+                });
+                setTimeout(() => res(), 400);
+            }),
+            enter: ({next}) => new Promise((res, rej) => {
+                const mult = next.namespace === 'home' ? -1 : 1;
+                gsap.fromTo(next.container.querySelector('.global-nav'), .2, {
+                    y: -50,
+                    opacity: 0
+                }, {
+                    y: 0,
+                    opacity: 1
+                });
+                gsap.fromTo(next.container.querySelectorAll('.content-block-header > *'), .3, {
+                    x: 100 * mult,
                     opacity: 0
                 }, {
                     x: 0,
                     opacity: 1,
+                    stagger: .1,
                     ease: Power2.easeOut
                 });
-            }
-        }]
+                gsap.fromTo(next.container.querySelector('.content-bg'), .3, {
+                    opacity: 0
+                }, {
+                    opacity: 1
+                });
+                setTimeout(() => res(), 300);
+            })
+        }
+    ]
     });
 });
